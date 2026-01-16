c43b6c9409ccd2aeb4f6084f0044b0e32fcc5e12
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-01-16 23:38:47 +0100

    2. output

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello World!");
+﻿Console.Write("Hi!");
+Console.WriteLine("Hello!");
+
+Console.WriteLine("\tnemanjatat");
+
+// This is a comment
+
+/*
+ * this
+ * is
+ * a
+ * multiline
+ * comment
+ */
+
+Console.ReadKey();
+
+Console.WriteLine("Movies are great...\nand so is swimming!");
+
+Console.ReadKey();
