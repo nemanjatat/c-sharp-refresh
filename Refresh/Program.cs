310d16f35b802e874e93b817bdead88c1045756d
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-01-26 16:49:14 +0100

    4. constants

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -1,28 +1,8 @@
-﻿int x; // declaration
-x = 123; // initialization
+﻿// constants    =   immutalble values which are known at compile time
+//                  and do not change for the life of the program
 
-int y = 321; // declaration + initialization
+const double pi = 3.14159;
 
-int z = x + y;
+Console.WriteLine(pi);
 
-Console.WriteLine($"{x} + {y} = {z}");
-
-int age = 63; // integer = whole number
-double price = 14.99; // decimal number
-bool onSale = false; // boolean = true or false
-char symbol = '@'; // single character
-String name = "Tom"; // text
-
-Console.WriteLine($"Your age is: {age}");
-Console.WriteLine($"The price for that item is: {price}");
-Console.WriteLine($"Item on sale status: {onSale}");
-Console.WriteLine($"Chosen symbol: {symbol}");
-Console.WriteLine($"Your name is: {name}");
-
-Console.ReadKey();
-
-String username = symbol + name.ToLower();
-
-Console.WriteLine($"Your username is: {username}");
-
-Console.ReadKey();
+Console.ReadLine();
