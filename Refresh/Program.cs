8acd20b6bc3f372377632f16476652ad6bf1a9ca
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-01-27 16:55:30 +0100

    6. user input

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -1,32 +1,10 @@
-﻿// type casting =   Converting a value to a different data type
-//                  Useful when we accept uset input (string)
-//                  Different data types can do different things
+﻿Console.WriteLine("What's your name?");
+String name = Console.ReadLine();
 
-double a = 3.14;
-int b = Convert.ToInt32(a);
+Console.WriteLine("What's your age?");
+int age = Convert.ToInt32(Console.ReadLine());
 
-int c = 1246;
-double d = Convert.ToDouble(c) + 0.1; // so it'd show decimals
-
-int e = 123;
-String f = Convert.ToString(e);
-
-String g = "@";
-char h = Convert.ToChar(g);
-
-String i = "true";
-bool j = Convert.ToBoolean(i);
-
-Console.WriteLine(b);
-Console.WriteLine(d);
-Console.WriteLine(f);
-Console.WriteLine(h);
-Console.WriteLine(j);
-
-Console.WriteLine(b.GetType());
-Console.WriteLine(d.GetType());
-Console.WriteLine(f.GetType());
-Console.WriteLine(h.GetType());
-Console.WriteLine(j.GetType());
+Console.WriteLine("Your name is: " + name);
+Console.WriteLine("You are " + age + " years old");
 
 Console.ReadKey();
