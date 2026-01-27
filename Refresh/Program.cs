113c60bcb4e7c9f02e1791b8f5e103aa46d0c772
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-01-27 16:31:20 +0100

    5. type casting

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -1,8 +1,32 @@
-﻿// constants    =   immutalble values which are known at compile time
-//                  and do not change for the life of the program
+﻿// type casting =   Converting a value to a different data type
+//                  Useful when we accept uset input (string)
+//                  Different data types can do different things
 
-const double pi = 3.14159;
+double a = 3.14;
+int b = Convert.ToInt32(a);
 
-Console.WriteLine(pi);
+int c = 1246;
+double d = Convert.ToDouble(c) + 0.1; // so it'd show decimals
 
-Console.ReadLine();
+int e = 123;
+String f = Convert.ToString(e);
+
+String g = "@";
+char h = Convert.ToChar(g);
+
+String i = "true";
+bool j = Convert.ToBoolean(i);
+
+Console.WriteLine(b);
+Console.WriteLine(d);
+Console.WriteLine(f);
+Console.WriteLine(h);
+Console.WriteLine(j);
+
+Console.WriteLine(b.GetType());
+Console.WriteLine(d.GetType());
+Console.WriteLine(f.GetType());
+Console.WriteLine(h.GetType());
+Console.WriteLine(j.GetType());
+
+Console.ReadKey();
