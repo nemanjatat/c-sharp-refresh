16664dbd0073deb969b89e9dd20fadb96c895f57
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-01-28 12:55:54 +0100

    8. math class

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -1,33 +1,39 @@
-﻿int numOfAvailableMovies = 5;
+﻿double x = 3;
+double y = -3;
+double z = 3.14;
+double w = 3.23;
+double t = 3.66;
+double n = 10;
+double m = 20;
 
-Console.WriteLine("-- addition --");
-numOfAvailableMovies = numOfAvailableMovies + 1;
-numOfAvailableMovies += 1;
-numOfAvailableMovies++;
-Console.WriteLine(numOfAvailableMovies);
-Console.ReadKey();
+double a = Math.Pow(x, 2);
+PrintAndRead(a);
 
-Console.WriteLine("-- subtraction --");
-numOfAvailableMovies = numOfAvailableMovies - 1;
-numOfAvailableMovies -= 1;
-numOfAvailableMovies--;
-Console.WriteLine(numOfAvailableMovies);
-Console.ReadKey();
+double b = Math.Sqrt(x);
+PrintAndRead(b);
 
-Console.WriteLine("-- multiplication --");
-numOfAvailableMovies = numOfAvailableMovies * 2;
-numOfAvailableMovies *= 2;
-Console.WriteLine(numOfAvailableMovies);
-Console.ReadKey();
+double c = Math.Abs(y);
+PrintAndRead(c);
 
-Console.WriteLine("-- division --");
-numOfAvailableMovies = numOfAvailableMovies / 2;
-numOfAvailableMovies /= 2;
-Console.WriteLine(numOfAvailableMovies);
-Console.ReadKey();
+double d = Math.Round(z);
+PrintAndRead(d);
 
-Console.WriteLine("-- modulus --");
-int remainder;
-remainder = numOfAvailableMovies % 2;
-Console.WriteLine(remainder);
-Console.ReadKey();
+double e = Math.Ceiling(w);
+PrintAndRead(e);
+
+double f = Math.Floor(t);
+PrintAndRead(f);
+
+double g = Math.Max(n, m);
+PrintAndRead(g);
+
+double h = Math.Min(n, m);
+PrintAndRead(h);
+
+
+
+static void PrintAndRead(double num)
+{
+    Console.WriteLine(num);
+    Console.ReadKey();
+}
