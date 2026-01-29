24ff2f7cb647c60d105e002f6e59feb308fe9258
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-01-29 15:24:04 +0100

    13. switches

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -1,41 +1,49 @@
-﻿// if statements
-// basic form of decision making
+﻿// switch = an efficient way to avoid writting too many if statements
 
-try
+Console.WriteLine("Would you like to manually set current day, " +
+    "or let the system do it automatically? (M/A)");
+String userChoice = Console.ReadLine().ToUpper();
+
+if (userChoice == "M")
 {
-    Console.Write("Enter your age: ");
-    int age = Convert.ToInt32(Console.ReadLine());
+    Console.Write("What day of the week is it: ");
+    String userDayOfTheWeek = Console.ReadLine().ToUpper();
 
-    if (age > 100)
+    switch (userDayOfTheWeek)
     {
-        Console.WriteLine("You are too old!");
+        case "MONDAY":
+            Console.WriteLine("Today is MONDAY!");
+            break;
+        case "TUESDAY":
+            Console.WriteLine("Today is TUESDAY");
+            break;
+        case "WEDNESDAY":
+            Console.WriteLine("Today is WEDNESDAY");
+            break;
+        case "THURSDAY":
+            Console.WriteLine("Today is THURSDAY");
+            break;
+        case "FRIDAY":
+            Console.WriteLine("Today is FRIDAY");
+            break;
+        case "SATURDAY":
+            Console.WriteLine("Today is SATURDAY");
+            break;
+        case "SUNDAY":
+            Console.WriteLine("Today is SUNDAY");
+            break;
+        default:
+            Console.WriteLine("I am confused");
+            break;
     }
-    else if (age >= 18)
-    {
-        Console.WriteLine("You are now signed up!");
-    }
-    else if (age < 0)
-    {
-        Console.WriteLine("You haven't been borned yet!");
-    }
-    else
-    {
-        Console.WriteLine("You must be 18 years or older to sign up!");
-    }
-
-    Console.WriteLine("=====================");
-    Console.WriteLine("=====================");
-    Console.WriteLine("=====================");
-
-    Console.Write("Enter your name: ");
-    String name = Console.ReadLine();
-
-    if (name == "")
-        Console.WriteLine("You did not enter your name!");
-    else
-        Console.WriteLine($"Hello {name}!");
 }
-catch(Exception e)
+else if (userChoice == "A")
 {
-    Console.WriteLine("Please enter a number and nothing else!\n---ERROR MESSAGE---\n" + e);
+    Console.WriteLine($"Today is {DateTime.Now.DayOfWeek.ToString().ToUpper()}");
 }
+else
+{
+    Console.WriteLine("Huh?");
+}
+
+Console.ReadKey();
