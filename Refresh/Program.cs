2b786b7ca22e574fcec5e61514b90ab1ae65ebf7
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-01-29 15:05:08 +0100

    12. if statements

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -1,97 +1,41 @@
-﻿List<String> choices = new List<string> { "rock", "paper", "scissors" };
-Random random = new Random();
-int playerPoints, opponentPoints;
-playerPoints = 0;
-opponentPoints = 0;
+﻿// if statements
+// basic form of decision making
 
-while (true)
+try
 {
-    try
+    Console.Write("Enter your age: ");
+    int age = Convert.ToInt32(Console.ReadLine());
+
+    if (age > 100)
     {
-        Console.Write("Rock, paper or scissors: ");
-        String playerChoice = Console.ReadLine().ToLower();
-
-        String opponentChoice = choices[random.Next(0, 3)];
-
-        Console.WriteLine($"Player chose: {playerChoice}\nOpponent chose: {opponentChoice}");
-
-        if (playerChoice == "rock")
-        {
-            switch (opponentChoice)
-            {
-                case "scissors":
-                    Console.WriteLine("Player Wins!");
-                    playerPoints++;
-                    break;
-                case "paper":
-                    Console.WriteLine("Player Loses!");
-                    opponentPoints++;
-                    break;
-                case "rock":
-                    Console.WriteLine("It's a tie!");
-                    break;
-                default:
-                    break;
-            }
-        }
-        else if (playerChoice == "paper")
-        {
-            switch (opponentChoice)
-            {
-                case "rock":
-                    Console.WriteLine("Player Wins!");
-                    playerPoints++;
-                    break;
-                case "scissors":
-                    Console.WriteLine("Player Loses!");
-                    opponentPoints++;
-                    break;
-                case "paper":
-                    Console.WriteLine("It's a tie!");
-                    break;
-                default:
-                    break;
-            }
-        }
-        else if (playerChoice == "scissors")
-        {
-            switch (opponentChoice)
-            {
-                case "paper":
-                    Console.WriteLine("Player Wins!");
-                    playerPoints++;
-                    break;
-                case "rock":
-                    Console.WriteLine("Player Loses!");
-                    opponentPoints++;
-                    break;
-                case "scissors":
-                    Console.WriteLine("It's a tie!");
-                    break;
-                default:
-                    break;
-            }
-        }
-        else
-        {
-            Console.WriteLine("Unknown command");
-        }
-
-        Console.WriteLine($"Player points: {playerPoints}\nOpponent points: {opponentPoints}");
-        Console.Write("Would you like to continue playing? (Y/N): ");
-        String continuePlaying = Console.ReadLine().ToUpper();
-        
-        if (continuePlaying == "Y")
-        {
-            continue;
-        }
-        else
-        {
-            break;
-        }
+        Console.WriteLine("You are too old!");
     }
-    catch (Exception e)
+    else if (age >= 18)
     {
-        Console.WriteLine(e.Message);
+        Console.WriteLine("You are now signed up!");
     }
+    else if (age < 0)
+    {
+        Console.WriteLine("You haven't been borned yet!");
+    }
+    else
+    {
+        Console.WriteLine("You must be 18 years or older to sign up!");
+    }
+
+    Console.WriteLine("=====================");
+    Console.WriteLine("=====================");
+    Console.WriteLine("=====================");
+
+    Console.Write("Enter your name: ");
+    String name = Console.ReadLine();
+
+    if (name == "")
+        Console.WriteLine("You did not enter your name!");
+    else
+        Console.WriteLine($"Hello {name}!");
+}
+catch(Exception e)
+{
+    Console.WriteLine("Please enter a number and nothing else!\n---ERROR MESSAGE---\n" + e);
 }
