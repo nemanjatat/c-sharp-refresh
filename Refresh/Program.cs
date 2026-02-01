f097219691c783501214204af92a2c2694720102
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-01 15:53:21 +0100

    24. return keyword

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -1,24 +1,19 @@
-﻿// method = performs a section of code, whenever it's called (invoked)
-//          benefit = let's us reuse code without writing it multiple times
+﻿// return = returns data back to the place where a method is invoked
 
-Console.Write("Enter your name: ");
-String name = Console.ReadLine();
+Console.Write("Enter number 1: ");
+double x = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Enter your age: ");
-int age = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number 2: ");
+double y = Convert.ToDouble(Console.ReadLine());
 
-singHappyBday(name, age);
-singHappyBday(name, age);
-singHappyBday(name, age);
+double result = Multiply(x, y);
+
+Console.WriteLine($"{x} * {y} = {result}");
 
 Console.ReadKey();
 
-static void singHappyBday(String birthdayBoy, int yearsOld)
+static double Multiply(double x, double y)
 {
-    Console.WriteLine("Happy birthday to you!");
-    Console.WriteLine("Happy birthday to you!");
-    Console.WriteLine($"Happy birthday dear {birthdayBoy}!");
-    Console.WriteLine($"You are {yearsOld} years old!");
-    Console.WriteLine("Happy birthday to you!");
-    Console.WriteLine();
+    double result = x * y;
+    return result;
 }
