445e9020460b2a4d807392f58b141fe009ea811d
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-01 16:09:15 +0100

    25. method overloading

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -1,18 +1,39 @@
-﻿// return = returns data back to the place where a method is invoked
+﻿// method overloading = methods share same name, but different parameters
+//                      name + parameters = signature
+//                      methods must have a unique signature
 
-Console.Write("Enter number 1: ");
-double x = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Enter number 2: ");
-double y = Convert.ToDouble(Console.ReadLine());
-
-double result = Multiply(x, y);
-
-Console.WriteLine($"{x} * {y} = {result}");
-
-Console.ReadKey();
-
-static double Multiply(double x, double y)
+class Program
 {
-    return x * y;
+    static void Main()
+    {
+        Console.Write("Enter number 1: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter number 2: ");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter number 3: ");
+        double z = Convert.ToDouble(Console.ReadLine());
+
+        double result = Multiply(x, y);
+        Console.WriteLine($"{x} * {y} = {result}");
+
+        result = Multiply(x, y, z);
+        Console.WriteLine($"{x} * {y} * {z} = {result}");
+
+        Console.ReadKey();
+    }
+
+    static double Multiply(double x, double y)
+    {
+        return x * y;
+    }
+
+    static double Multiply(double x, double y, double z)
+    {
+        return x * y * z;
+    }
 }
+
+
+
