6bfb39ebafd5e5dfd187306c6d5847561ad1d59f
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-04 13:31:07 +0100

    29. string interpolation

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -1,19 +1,22 @@
-﻿// conditional operator = used in conditional assignment if a condition is true/false
+﻿// string interpolation = allows us to insert variables into a string literal
+//                        precede a string literal with $
+//                        {} are placeholders
 
-// (condition) ? x : y
+String name, destination;
 
-var availableMovies = new List<String>
+Console.Write("What's your name: ");
+name = Console.ReadLine();
+
+Console.Write($"It's nice to meet you {name}. Where would you like to go: ");
+destination = Console.ReadLine();
+
+Console.Write($"Good news {name}");
+for(int i = 0; i < 3; i++)
 {
-    "THE LAST SAMURAI",
-    "GOOD WILL HUNTING",
-    "STAR WARS"
-};
-
-Console.Write("Which movie are you looking for: ");
-String movieSelect = Console.ReadLine().ToUpper();
-
-var message = (availableMovies.Exists(movie => movie == movieSelect)) ? "That movie is available" : "That movie is NOT available!";
-
-Console.WriteLine(message);
+    Console.Write(".");
+    Thread.Sleep(1000);
+}
+Console.WriteLine();
+Console.WriteLine($"You are heading to {destination}!");
 
 Console.ReadKey();
