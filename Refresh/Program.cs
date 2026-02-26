4e0d1a8b749d58feac432d31e87035edb665c924
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-26 14:05:59 +0100

    30. multidimensional arrays pt2

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -9,7 +9,18 @@ String[,] parkingLot = { { "Focus", "EcoSport", "Kuga" },
                          { "Astra", "Insignia", "Corsa" }
                        };
 
+/*
 foreach (String car in parkingLot)
     Console.WriteLine(car);
+*/
+
+for (int i = 0; i < parkingLot.GetLength(0); i++)
+{
+    for (int j = 0; j < parkingLot.GetLength(1); j++)
+    {
+        Console.Write(parkingLot[i, j] + " ");
+    }
+    Console.WriteLine();
+}
 
 Console.ReadKey();
