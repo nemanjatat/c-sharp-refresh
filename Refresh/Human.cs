bbf1dec6132257f94433c65590730e65acd1a59d
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-28 19:22:17 +0100

    32. objects

---
+++ Refresh/Human.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refresh
+{
+    internal class Human
+    {
+        // characteristics
+        public String name;
+        public int age;
+
+        // actions
+        public void Eat()
+        {
+            Console.WriteLine($"{name} is eating");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine($"{name} is sleeping");
+        }
+    }
+}
--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -4,12 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        // class = A bundle of related code.
-        //         Can be used as a blueprint to create objects (OOP)
+        // object = An instance of a class
+        //          A class can be used as a blueprint to create objects (OOP)
+        //          Objects can have fields & methods (characteristics & actions)
 
-        Messages.Hello();
-        Messages.Waiting();
-        Messages.Bye();
+        Human human1 = new Human();
+        Human human2 = new Human();
+
+        human1.name = "Rick";
+        human1.age = 65;
+
+        human2.name = "Morty";
+        human2.age = 16;
+
+        human1.Eat();
+        human1.Sleep();
+        
+        human2.Eat();
+        human2.Sleep();
 
         Console.ReadKey();
     }
