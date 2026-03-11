167e8c5d16bed6adaec781526b0876e558664578
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-03-11 16:11:50 +0100

    37. abstract classes

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        // inheritance = 1 or more child classes receiving fields, methods, etc. from a common parent
-
+        // abstract classes = modifier that indicates missing components or incomplete implementation
+        
         Car car = new Car();
         Bicycle bicycle = new Bicycle();
         Boat boat = new Boat();
--- Refresh/Vehicle.cs
+++ Refresh/Vehicle.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Refresh
 {
-    internal class Vehicle
+    // We don't want users to be able to create a generic Vehicle object...
+    // ... as it's missing the wheels property
+    // so we'll add the abstract modifier to the class so that it can't be used
+    // with that, the users can't do:
+    // Vehicle vehicle = new Vehicle();
+    abstract internal class Vehicle
     {
         public int speed;
 
