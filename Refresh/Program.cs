91adbdca743658779418762113fa25688bade675
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-03-14 10:44:34 +0100

    38. array of objects pt3

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -4,15 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Car[] garage = new Car[3];
-
-        Car car1 = new Car("Mustang");
-        Car car2 = new Car("Corvette");
-        Car car3 = new Car("Lambo");
-
-        garage[0] = car1;
-        garage[1] = car2;
-        garage[2] = car3;
+        Car[] garage =
+        {
+            new Car("Mustang"),
+            new Car("Corvette"),
+            new Car("Lambo")
+        };
 
         foreach(Car car in garage)
         {
