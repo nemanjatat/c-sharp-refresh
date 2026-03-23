853b971f0b27638591b91fb264ee3aeaf2087c72
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-03-23 12:13:49 +0100

    45. list of objects pt1

---
+++ Refresh/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refresh
+{
+    internal class Movie
+    {
+        public String name;
+
+        public Movie(String name)
+        {
+            this.name = name;
+        }
+    }
+}
--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -5,35 +5,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        // List = data structure that represents a list of objects that can be accessed by index.
-        //        Similar to array, but can dynamically increase/decrease in size
-        //        using System.Collections.Generic;
+        List<Movie> movies = new List<Movie>();
 
-        List<String> menu = new List<String>();
+        Movie movie1 = new Movie("Project Hail Mary".ToLower());
+        Movie movie2 = new Movie("Knowing".ToLower());
+        Movie movie3 = new Movie("The Grand Budapest Hotel".ToLower());
 
-        menu.Add("govedja bistra supa");
-        menu.Add("engleski dorucak");
-        menu.Add("kroasan nutela");
-        menu.Add("pohovani pileci stapici mala porcija");
+        movies.Add(movie1);
+        movies.Add(movie2);
+        movies.Add(movie3);
 
-        //menu.Remove("engleski dorucak");
-        //menu.Insert(0, "biftek natur");
-        //Console.WriteLine(menu.Count());
-        //Console.WriteLine(menu.IndexOf("kroasan nutela"));
-        //menu.Add("kroasan nutela"); Console.WriteLine(menu.LastIndexOf("kroasan nutela"));
-        //Console.WriteLine(menu.Contains("engleski dorucak"));
-        //menu.Sort();
-        //menu.Reverse();
-        /*  String[] menuArray = menu.ToArray();
-            foreach (String menuItem in menuArray)
-            {
-                Console.WriteLine(menuItem);
-            }
-        */
-
-        foreach (String item in menu)
+        foreach (Movie movie in movies)
         {
-            Console.WriteLine(item);
+            Console.WriteLine(movie.name);
         }
 
         Console.ReadLine();
