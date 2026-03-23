07869ac5cff358f5c857dab86713079b15178d8e
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-03-23 12:02:09 +0100

    44. lists

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -1,24 +1,40 @@
 ﻿using Refresh;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // interface = defines a "contract" that all the class inheriting from should follow
+        // List = data structure that represents a list of objects that can be accessed by index.
+        //        Similar to array, but can dynamically increase/decrease in size
+        //        using System.Collections.Generic;
 
-        //             An interface declares "what a class should have"
-        //             An inheriting class defines "how it should do it"
+        List<String> menu = new List<String>();
 
-        //             Benefit = security + multiple inheritence + "plug-and-play"
+        menu.Add("govedja bistra supa");
+        menu.Add("engleski dorucak");
+        menu.Add("kroasan nutela");
+        menu.Add("pohovani pileci stapici mala porcija");
 
-        Rabbit rabbit = new Rabbit();
-        Hawk hawk = new Hawk();
-        Fish fish = new Fish();
+        //menu.Remove("engleski dorucak");
+        //menu.Insert(0, "biftek natur");
+        //Console.WriteLine(menu.Count());
+        //Console.WriteLine(menu.IndexOf("kroasan nutela"));
+        //menu.Add("kroasan nutela"); Console.WriteLine(menu.LastIndexOf("kroasan nutela"));
+        //Console.WriteLine(menu.Contains("engleski dorucak"));
+        //menu.Sort();
+        //menu.Reverse();
+        /*  String[] menuArray = menu.ToArray();
+            foreach (String menuItem in menuArray)
+            {
+                Console.WriteLine(menuItem);
+            }
+        */
 
-        rabbit.Flee();
-        hawk.Hunt();
-        fish.Flee();
-        fish.Hunt();
+        foreach (String item in menu)
+        {
+            Console.WriteLine(item);
+        }
 
         Console.ReadLine();
     }
