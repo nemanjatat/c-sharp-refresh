f3195923c4b463712d2f8024d51dea219b32abb7
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-03-31 18:23:06 +0200

    48. enums pt2

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -11,21 +11,22 @@ class Program
 
         //         name = integer
 
-        Console.WriteLine($"{Planets.Pluto} is a planet.");
+        Console.WriteLine($"{Planets.Pluto} is planet #{(int)Planets.Pluto}.");
+        Console.WriteLine($"{Planets.Mercury} is planet #{(int)Planets.Mercury}.");
 
         Console.ReadLine();
     }
 
     enum Planets
     {
-        Mercury,
-        Venus,
-        Earth,
-        Mars,
-        Jupiter,
-        Saturn,
-        Uranus,
-        Neptune,
-        Pluto
+        Mercury = 1,
+        Venus = 2,
+        Earth = 3,
+        Mars = 4,
+        Jupiter = 5,
+        Saturn = 6,
+        Uranus = 7,
+        Neptune = 8,
+        Pluto = 9
     }
 }
